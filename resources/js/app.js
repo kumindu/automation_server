031cd0e186e55f792039ef7fa120a11c8c2f1370
kajasumanie <kirlogapanet@gmail.com>
2021-03-26 15:56:44 +0530

    update repo

--- resources/js/app.js
+++ resources/js/app.js
@@ -8,20 +8,6 @@ require('./bootstrap');
 
 window.Vue = require('vue');
 
-Vue.component(
-    'passport-clients',
-    require('./components/passport/Clients.vue').default
-);
-
-Vue.component(
-    'passport-authorized-clients',
-    require('./components/passport/AuthorizedClients.vue').default
-);
-
-Vue.component(
-    'passport-personal-access-tokens',
-    require('./components/passport/PersonalAccessTokens.vue').default
-);
 /**
  * The following block of code may be used to automatically register your
  * Vue components. It will recursively scan this directory for the Vue
@@ -30,8 +16,8 @@ Vue.component(
  * Eg. ./components/ExampleComponent.vue -> <example-component></example-component>
  */
 
-// const files = require.context('./', true, /\.vue$/i);
-// files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default));
+// const files = require.context('./', true, /\.vue$/i)
+// files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 
 Vue.component('example-component', require('./components/ExampleComponent.vue').default);
 
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">Dashboard</div>
+                <div class="card-header">{{ __('Dashboard') }}</div>
 
                 <div class="card-body">
                     @if (session('status'))
@@ -14,15 +14,9 @@
                         </div>
                     @endif
 
-                    You are logged in!
+                    {{ __('You are logged in!') }}
                 </div>
             </div>
-            </br> 
-            <passport-clients></passport-clients>
-            </br> 
-            <passport-authorized-clients></passport-authorized-clients>
-            </br> 
-            <passport-personal-access-tokens></passport-personal-access-tokens>
         </div>
     </div>
 </div>
